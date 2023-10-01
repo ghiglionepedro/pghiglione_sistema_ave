171da0be79b321d04777bb68969b37df3b7b5ec9
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2023-10-01 19:29:53 -0300

    Cambios a ser confirmados: 	nuevos archivos: pghiglione_mascotas/__init__.py 	nuevos archivos: pghiglione_mascotas/__manifest__.py 	nuevos archivos: pghiglione_mascotas/models/__init__.py 	nuevos archivos: pghiglione_mascotas/models/mascota.py 	nuevos archivos: pghiglione_mascotas/static/description/icon.png 	nuevos archivos: pghiglione_mascotas/static/img/avatar.png 	nuevos archivos: pghiglione_mascotas/views/mascota.xml 	nuevos archivos: pghiglione_mascotas/views/menu.xml

---
+++ pghiglione_mascotas/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Mascotas",
+    "version": "15.0.0.0.0",
+    "license": "AGPL-3",
+    "summary": "Adminsitración de mascotas",
+    "author": "Ghiglione Pedro Matias",
+    "maintainer": "Ghiglione Pedro Matias",
+    "website": "https://quimerasoftware.ar",
+    "depends": ["partner"],
+    "data": [
+        "views/mascota.xml",
+        "views/menu.xml",
+    ],
+    "application": True,
+    "development_status": "Beta",
+    "maintainers": ["pghiglione"],
+}
---
+++ pghiglione_mascotas/models/mascota.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class mascota(models.Model):
+    _name = "mascota"
+    _description = "Mascota"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _order = "name"
+
+    especie = fields.Char()
+    raza = fields.Char()
+    nombre = fields.Char()
+    sexo = fields.Char()
+    edad = fields.Char()
+    pelaje = fields.Char()
+    image = fields.Binary(
+        attachment=True, help="Imagen de mascota"
+    )
