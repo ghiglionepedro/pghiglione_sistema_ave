e12c2f77a6b1ea4417647932e5d746f2a27ffd09
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2023-10-01 19:34:20 -0300

    Cambios a ser confirmados: 	modificados:     sistema_ave_mascotas/__manifest__.py

--- sistema_ave_mascotas/__manifest__.py
+++ sistema_ave_mascotas/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "Ghiglione Pedro Matias",
     "maintainer": "Ghiglione Pedro Matias",
     "website": "https://quimerasoftware.ar",
-    "depends": ["partner"],
+    "depends": ["contacts"],
     "data": [
         "views/mascota.xml",
         "views/menu.xml",
