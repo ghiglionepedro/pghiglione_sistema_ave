eda86446f1f732c0783a1838e1236dad0fc255a5
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2023-10-01 18:35:21 -0300

    Cambios a ser confirmados: 	nuevos archivos: sistema_ave_res_partner/models/__init__.py 	nuevos archivos: sistema_ave_res_partner/models/models.py

---
+++ sistema_ave_res_partner/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class /opt/odoo15/odoo/repositorios/adrian-pet/seller_in_product_template(models.Model):
+#     _name = '/opt/odoo15/odoo/repositorios/adrian-pet/seller_in_product_template./opt/odoo15/odoo/repositorios/adrian-pet/seller_in_product_template'
+#     _description = '/opt/odoo15/odoo/repositorios/adrian-pet/seller_in_product_template./opt/odoo15/odoo/repositorios/adrian-pet/seller_in_product_template'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
