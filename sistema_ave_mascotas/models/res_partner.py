5e630f53792306bd133b56ddef72613e1404e8fa
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2023-10-01 23:50:52 -0300

    Cambios a ser confirmados: 	modificados:     sistema_ave_mascotas/models/res_partner.py 	modificados:     sistema_ave_res_partner/views/res_partner.xml

--- sistema_ave_mascotas/models/res_partner.py
+++ sistema_ave_mascotas/models/res_partner.py
@@ -12,16 +12,16 @@ class ResPartner(models.Model):
             rec.animal_count = len(rec.mascota_ids)
 
     mascota_ids = fields.One2many("mascota", "partner_id", string="Mascotas")
-    animal_count = fields.Integer(
+    mascotas_count = fields.Integer(
         compute=_compute_animal_count, string="Número de mascotas", store=True
     )
 
-    def action_view_animals(self):
-        xmlid = "mascotas.action_mastoca"
+    def action_view_mascotas(self):
+        xmlid = "sistema_ave_mascotas.action_mascota"
         action = self.env["ir.actions.act_window"]._for_xml_id(xmlid)
         if self.animal_count > 1:
             action["domain"] = [("id", "in", self.mascota_ids.ids)]
         else:
-            action["views"] = [(self.env.ref("animal.view_animal_form").id, "form")]
+            action["views"] = [(self.env.ref("sistema_ave_mascotas.view_mascota_form").id, "form")]
             action["res_id"] = self.mascota_ids and self.mascota_ids.ids[0] or False
         return action
