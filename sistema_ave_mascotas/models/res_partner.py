51436a4752be453d5189452b6ecd53627a5e72cb
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2023-10-06 01:42:00 -0300

    Cambios a ser confirmados: 	modificados:     sistema_ave_mascotas/models/res_partner.py

--- sistema_ave_mascotas/models/res_partner.py
+++ sistema_ave_mascotas/models/res_partner.py
@@ -6,10 +6,6 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    mascota_ids = fields.One2many("mascota", "partner_id", string="Mascotas")
-    mascotas_count = fields.Integer(
-        compute=_compute_animal_count(), string="Número de mascotas", store=True
-    )
 
     @api.depends("mascota_ids")
     def _compute_animal_count(self):
@@ -25,3 +21,6 @@ class ResPartner(models.Model):
             action["views"] = [(self.env.ref("sistema_ave_mascotas.view_mascota_form").id, "form")]
             action["res_id"] = self.mascota_ids and self.mascota_ids.ids[0] or False
         return action
+
+    mascota_ids = fields.One2many("mascota", "partner_id", string="Mascotas")
+    mascotas_count = fields.Integer(compute=_compute_animal_count, string="Número de mascotas", store=True)
